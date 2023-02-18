c9d652466ef4e3d74ea4fee9207c5a35543ebcfd
Konstantinos Nikopoulos <knikopoulos3@gmail.com>
2023-02-18 02:13:01 +0200

    infinite scroll

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,26 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked , HostListener,ElementRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from '../image.service';
 
 import { IImage } from '../core/interfaces/image.interface';
 import { Subscription } from 'rxjs';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit, OnDestroy {
+export class GalleryComponent implements OnInit,AfterViewChecked, OnDestroy {
+
+
+  @ViewChild('spinner', {static: false}) spinner: MatProgressSpinner;
+  isSpinnerScrolledIntoView: boolean = false;
+  loading: boolean = false;
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView(){
+    this.checkScrollSpinner();
+    
+  }
 
   images: IImage[]= [];
   imagesSubscription: Subscription ;
@@ -19,14 +30,18 @@ export class GalleryComponent implements OnInit, OnDestroy {
     this.imagesSubscription = this.imageService.loadedImages$.subscribe((res)=> {
       this.images = res.data;
       this.count = res.count;
-      console.log(this.count == this.images.length);
+      this.loading = false;
     });
     //PUT THIS IN INFINITE SCROLL COMPONENT
-    this.loadNewImages();
+    if(this.images.length == 0)
+      this.loadNewImages();
   }
 
   ngOnInit(): void {
+  }
 
+  ngAfterViewChecked(): void {
+    this.checkScrollSpinner();
     
   }
  
@@ -44,6 +59,25 @@ export class GalleryComponent implements OnInit, OnDestroy {
     
     this.imageService.loadImages();
     
+  }
+
+  checkScrollSpinner() {
+    
+    if (this.spinner){
+      const rect = this.spinner._elementRef.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      
+      const bottomShown = rect.bottom-60 <= window.innerHeight;
+      this.isSpinnerScrolledIntoView = topShown && bottomShown;
+      // console.log(topShown, bottomShown, rect.bottom, window.innerHeight);
+      
+      if(this.isSpinnerScrolledIntoView && !this.loading) {
+        this.loading = true;
+        this.loadNewImages();
+        this.isSpinnerScrolledIntoView=false;
+      }
+      
+    }
   }
   
 }
